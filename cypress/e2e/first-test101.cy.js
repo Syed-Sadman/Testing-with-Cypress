2b91daba93cef2064c83e6aeb21646ede21815ce
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2022-07-31 19:20:18 +0600

    subscription form test with cypress

--- cypress/e2e/first-test101.cy.js
+++ cypress/e2e/first-test101.cy.js
@@ -1,5 +1,12 @@
-describe("empty spec", () => {
-  it("passes", () => {
-    cy.visit("https://calendartest.dorik.io/");
+describe("My First Test", () => {
+  it('clicks the link "type"', () => {
+    cy.visit("https://sqasyedtest101.dorik.io");
+    for (let i = 0; i < 2; i++) {
+      cy.get("form").eq(i).type("fayhhynybygvteected@gmail.com").submit();
+    }
+
+    // cy.contains("Cypresslink101").click();
   });
 });
+
+// login auth
